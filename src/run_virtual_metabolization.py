2229f3fb2b2d049dc6900d4fd711b2f8782dbbbb
Lucas Pradi <155846495+lucaspradi@users.noreply.github.com>
2024-01-29 15:00:11 +0100

    Updated run_virtual_metabolization for better readability. Padronized all comments.

--- src/run_virtual_metabolization.py
+++ src/run_virtual_metabolization.py
@@ -1,3 +1,4 @@
+#Library imports
 import pandas as pd
 import numpy as np
 import math
@@ -12,14 +13,13 @@ import traceback
 import requests
 import pkg_resources
 import zipfile
+import os
+import sys
 
 lg = RDLogger.logger()
 lg.setLevel(RDLogger.ERROR)
 
-import os
-import sys
-
-# Function to check and install packages
+#Function to check for the presence of a package and install it if not present.
 def install_package(package):
     try:
         pkg_resources.require(package)
@@ -28,14 +28,20 @@ def install_package(package):
         print(f"{package} not found. Installing...")
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# Import and install SyGMa
+#Ensure the 'sygma' package is installed as it's crucial for GNPS data processing
 install_package("sygma")
 
-# Import SyGMa after ensuring it's installed
+#Import SyGMa after ensuring it's installed
 import sygma
 
 #Write out the results
 def export_for_SIRIUS(input_df_name, compound_name):
+    """
+    Export metabolite data for SIRIUS processing.
+    Handles data differently based on the source (SyGMa or BioTransformer).
+    Drops duplicate entries and exports the resulting DataFrame to a TSV file.
+    """
+
     df = pd.read_csv(input_df_name, sep='\t')
     print('==== Export for SIRIUS ====')
     print('Number of metabolites = ' +str(df.shape[0]))
@@ -61,6 +67,12 @@ def export_for_SIRIUS(input_df_name, compound_name):
     
 
 def export_for_NAP(input_df_name, compound_name):
+    """
+    Export metabolite data for NAP processing.
+    Handles data differently based on the source (SyGMa or BioTransformer).
+    Drops duplicate entries and exports the resulting DataFrame to a TSV file.
+    """
+
     df = pd.read_csv(input_df_name, sep='\t')
     print('==== Export for NAP ====')
     print('Number of metabolites = ' +str(df.shape[0]))  
@@ -84,33 +96,38 @@ def export_for_NAP(input_df_name, compound_name):
     print('Number of unique metabolites considered = ' +str(df_new.shape[0]))   
     df_new.to_csv(input_df_name[:-4]+'_NAP.tsv', sep = '\t', index = False, header= False)
 
-# Run SyGMa on two lists of SMILES and compound name in batch
+#Run SyGMa on two lists of SMILES and compound name in batch
 def run_sygma_batch(list_smiles, list_compound_name, phase_1_cycle, phase_2_cycle, top_sygma_candidates, output_name, compound_name):
+    """
+    Process a list of SMILES strings through SyGMa to predict metabolites.
+    Handles metabolite processing in batches and exports the results.
+    Also, prepares the data for further processing by SIRIUS and NAP.
+    """
     
     #Define the batch_size based on number of phase
     batch_size = int(100/(math.exp(phase_1_cycle))/(math.exp(phase_2_cycle)))
     
-    print('#### Starting SyGMa computation')
+    print('####Starting SyGMa computation')
     print('Number of compounds that will be subject to SyGMa metabolisation = '+str(len(list_smiles)))
     print('Number of phase I cycles = '+str(phase_1_cycle,)+'; Number of phase II cycles = '+str(phase_2_cycle)) 
     print('Keep only the top SyGMa candidates = '+str(top_sygma_candidates))
     #print('If you are running many compounds or cycles, and maxing out RAM memory available, you can decrease the batch size. Otherwise the value can be increased for faster computation.')
 
-    # Define SyGMa parameters      
+    #Define SyGMa parameters      
     scenario = sygma.Scenario([
         [sygma.ruleset['phase1'], int(phase_1_cycle)],
         [sygma.ruleset['phase2'], int(phase_2_cycle)]
     ])
     
-    # Create placeholder dataframe file
+    #Create placeholder dataframe file
     df_master = pd.DataFrame(columns=['metabolite', 'score', 'parent', 'pathway', 'Compound_Name'])
 
-    # Creating batches
+    #Creating batches
     for i in range(0, len(list_smiles), batch_size):
         list_smiles_batch = list_smiles[i:i + batch_size]
         list_compound_name_batch = list_compound_name[i:i + batch_size]
 
-        # Running SyGMa on each batch
+        #Running SyGMa on each batch
         for a, b in zip(list_smiles_batch, list_compound_name_batch):
             try:
                 mol = Chem.MolFromSmiles(a)
@@ -120,7 +137,7 @@ def run_sygma_batch(list_smiles, list_compound_name, phase_1_cycle, phase_2_cycl
                 metabolites = metabolic_tree.to_smiles()
                 metabolite_info = metabolic_tree.to_list()
 
-                # Extract metabolite data and create a DataFrame
+                #Extract metabolite data and create a DataFrame
                 metabolite_data = [{
                     'metabolite': met[0],
                     'score': round(met[1], 3) if met[1] != 'N/A' else 'N/A',
@@ -129,26 +146,26 @@ def run_sygma_batch(list_smiles, list_compound_name, phase_1_cycle, phase_2_cycl
                     'Compound_Name': b
                 } for met, info in zip(metabolites, metabolite_info)]
 
-                # Limit to top candidates and remove all-NaN columns
+                #Limit to top candidates and remove all-NaN columns
                 if metabolite_data:
-                    # Convert metabolite data to DataFrame and select top candidates
+                    #Convert metabolite data to DataFrame and select top candidates
                     df = pd.DataFrame(metabolite_data)
                     df = df.head(top_sygma_candidates + 1)
                     
-                    # Drop all-NA columns to clean data before concatenation
+                    #Drop all-NA columns to clean data before concatenation
                     df = df.dropna(axis=1, how='all')
                     
-                    # Ensure data types in 'df' match those in 'df_master' for consistency
+                    #Ensure data types in 'df' match those in 'df_master' for consistency
                     for column in df.columns:
                         if column in df_master.columns:
                             df[column] = df[column].astype(df_master[column].dtype)
                     
-                    # Concatenate 'df_master' with 'df', reindexing the result
+                    #Concatenate 'df_master' with 'df', reindexing the result
                     df_master = pd.concat([df_master, df], ignore_index=True)
 
             except Exception as e:
                 print(f"Error processing molecule: {a}. Error: {e}")
-                traceback.print_exc()  # Print the full traceback
+                traceback.print_exc()  #Print the full traceback
 
         print(f'Batch {i // batch_size + 1}/{math.ceil(len(list_smiles) / batch_size)} completed')
 
@@ -178,7 +195,8 @@ def run_sygma_batch(list_smiles, list_compound_name, phase_1_cycle, phase_2_cycl
 
 #Defining a function to check the Java version
 def check_java_version():
-    """Check and print the installed Java version."""
+    """Checks the current Java version to ensure compatibility with other components like BioTransformer."""
+
     try:
         java_version = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT)
         #print("Java version check completed.")
@@ -189,7 +207,7 @@ def check_java_version():
 
 #Defining a function to download and unzip the BioTransformer software if not already present
 def download_and_unzip_biotransformer():
-    """Download and unzip the BioTransformer software if not already present."""
+    """Downloads and extracts the BioTransformer tool if not already present. Ensures the necessary software is available for metabolite prediction."""
 
     url = 'https://bitbucket.org/wishartlab/biotransformer3.0jar/get/6432cf887ed70.zip'
     local_zip_file = 'biotransformer3.zip'
@@ -224,7 +242,8 @@ def download_and_unzip_biotransformer():
 
 #Prepare the execution environment for biotransformer3 by creating or cleaning the output folder
 def prepare_environment(source_dir, dest_dir):
-    """Prepare the working environment for biotransformer3 by moving files and cleaning up directories."""
+    """Prepares the working environment by moving files and renaming README to avoid conflicts with the current setup."""
+
     if os.path.exists(source_dir):
         readme_path = os.path.join(source_dir, "README.md")
         new_readme_path = os.path.join(source_dir, "README_BioTransformer.md")
@@ -240,6 +259,8 @@ def prepare_environment(source_dir, dest_dir):
 
 #Defining a function to check if the biotransformation type is valid
 def validate_biotransformation_type(type_of_biotransformation):
+    """Validates if the provided biotransformation type is among the recognized ones."""
+
     list_of_biotransformation = ['ecbased', 'cyp450', 'phaseII', 'hgut', 'superbio', 'allHuman', 'envimicro']
     if type_of_biotransformation in list_of_biotransformation:
         #print('     Biotransformation: '+type_of_biotransformation)
@@ -250,6 +271,8 @@ def validate_biotransformation_type(type_of_biotransformation):
 
 #Defining a function to create or clear the output folder        
 def create_or_clear_output_folder(folder_path):
+    "Creates the output folder if it doesn't exist or clears it if it does, preparing it for new output."
+
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     else:
@@ -264,39 +287,43 @@ def create_or_clear_output_folder(folder_path):
                 print(f'Failed to delete {file_path}. Reason: {e}')
 
 def prepare_for_bio3(type_of_biotransformation, list_smiles):
-    """Prepare the working environment for biotransformer3 by checking Java, Downloading and unziping BioTransformer3, prepare the folder, validate the biotransformation type, creating necessary files and cleaning up directories."""
+    """
+    Prepares the environment for BioTransformer processing.
+    Includes checking Java version, setting up BioTransformer, and validating inputs.
+    Ensures all prerequisites are met before starting the metabolite prediction.
+    """
 
     output_folder = "biotransformer_results"
 
-    # Check Java version
+    #Check Java version
     try:
         check_java_version()
         print(" > Java version check completed...")
     except Exception as e:
         print(f"Error checking Java version: {e}")
 
-    # Download and unzip BioTransformer
+    #Download and unzip BioTransformer
     try:
         download_and_unzip_biotransformer()
         print(" > BioTransformer download and unzip completed...")
     except Exception as e:
         print(f"Error downloading and unzipping BioTransformer: {e}")
 
-    # Prepare the working environment
+    #Prepare the working environment
     try:
         prepare_environment('wishartlab-biotransformer3.0jar-6432cf887ed7', '.',)
         print(" > BioTransformer environment preparation completed...")
     except Exception as e:
         print(f"Error preparing the working environment: {e}")
 
-    # Validate the biotransformation type
+    #Validate the biotransformation type
     try:
         validate_biotransformation_type(type_of_biotransformation)
         print(" > Biotransformation type validation completed...")
     except Exception as e:
         print(f"Error validating biotransformation type: {e}")
 
-    # Create the output folder if it doesn't exist, clear if it does
+    #Create the output folder if it doesn't exist, clear if it does
     try:
         create_or_clear_output_folder(output_folder)
         print(" > Output folder creation/clearing completed...")
@@ -308,13 +335,17 @@ def prepare_for_bio3(type_of_biotransformation, list_smiles):
     except FileNotFoundError:
         pass
 
-    print('######  Parameters checking for Biotransformer3 completed. Now starting the computation...')
-    print('######  Running BioTransformer takes approximatively 3-60 secs per compound depending on biotransformation parameters')
+    print('###### Parameters checking for Biotransformer3 completed. Now starting the computation...')
+    print('###### Running BioTransformer takes approximatively 3-60 secs per compound depending on biotransformation parameters')
     print('     Number of compounds being virtually metabolized with BioTransformer =  '+str(len(list_smiles)))
 
-# Run BioTransformer3 on two lists of SMILES and compound name - 2211
+#Run BioTransformer3 on two lists of SMILES and compound name - 2211
 def run_biotransformer3(mode, list_smiles, list_compound_name, type_of_biotransformation, number_of_steps, output_name):
-    """Run BioTransformer3 on SMILES string"""
+    """
+    Runs BioTransformer for a list of SMILES strings to predict metabolites.
+    Handles the execution of BioTransformer and processes the output, 
+    exporting results for further analysis and visualization.
+    """
 
     output_folder = "biotransformer_results"
     col_list = ['InChIKey',	'SMILES','PUBCHEM_CID','Molecular formula','Major Isotope Mass',
@@ -337,27 +368,27 @@ def run_biotransformer3(mode, list_smiles, list_compound_name, type_of_biotransf
             elif mode.str.startswith('-k'):
                 biotransformcall = 'java -jar BioTransformer3.0_20230525.jar -ismi "' + a +'" -ocsv '+str(output_filename)+' '+mode
 
-            biotransformcall = biotransformcall.split() # because call takes a list of strings 
+            biotransformcall = biotransformcall.split() #because call takes a list of strings 
             #call(biotransformcall)
             result = subprocess.run(biotransformcall, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
 
-            # Decode and process the output
+            #Decode and process the output
             stdout_decoded = result.stdout
             stderr_decoded = result.stderr
 
-            # Conditionally process stderr in case of error or unexpected message
+            #Conditionally process stderr in case of error or unexpected message
             if result.returncode != 0 or (stderr_decoded.strip() and "JAVA_TOOL_OPTIONS" not in stderr_decoded):
                 print("Error or unexpected message from subprocess:")
                 print(stderr_decoded)
 
-            # Parse the output and filter for the desired line
-            for line in stdout_decoded.splitlines():  # Change to stdout_decoded if needed
+            #Parse the output and filter for the desired line
+            for line in stdout_decoded.splitlines():  #Change to stdout_decoded if needed
                 if line.startswith("Processing molecule with SMILES:"):
                     print(line)
-                    break  # Stop after printing the first matching line
+                    break  #Stop after printing the first matching line
 
             try:
-                # Check if the output file exists and is not empty
+                #Check if the output file exists and is not empty
                 if os.path.exists(output_filename) and os.path.getsize(output_filename) > 0:
                     df_bio = pd.read_csv(output_filename, sep=',', usecols=col_list)
                     df_bio['Parent_Compound_Name'] = b
@@ -382,11 +413,11 @@ def run_biotransformer3(mode, list_smiles, list_compound_name, type_of_biotransf
             temp_df = pd.read_csv(temp_output_filename, sep='\t')
             dataframes.append(temp_df)
 
-    # Concatenate all DataFrames in the list
+    #Concatenate all DataFrames in the list
     df2_bio = pd.concat(dataframes, ignore_index=True)
 
     print(' - ')
-    print('####### VIRTUAL METABOLISATION COMPLETED FOR ALL COMPOUNDS #######')
+    print('#######VIRTUAL METABOLISATION COMPLETED FOR ALL COMPOUNDS #######')
     print('Total number of BioTransformer candidates = '+str(df2_bio.shape[0])+' - and '+str(len(df2_bio.SMILES.unique()))+" are unique")
     #print('Total number of unique BioTransformer candidates = '+str(len(df2_bio.SMILES.unique())))
 
@@ -403,7 +434,7 @@ def run_biotransformer3(mode, list_smiles, list_compound_name, type_of_biotransf
     print('  ')
     export_for_SIRIUS(run_biotransformer3.file_name_biotransf, 'Parent_Compound_Name')
     print('  ')
-    print('##### DOWNLOAD THE RESULTS BELOW #####')
+    print('#####DOWNLOAD THE RESULTS BELOW #####')
     run_biotransformer3.markdown_link_biotransf = f"[View/Download the vm_NAP results from {output_name}](./{run_biotransformer3.file_name_biotransf})."
     run_biotransformer3.markdown_link_biotransf_nap = f"[View/Download the vm_NAP BioTransformer results for Network Annotation Propagation from {output_name}](./{run_biotransformer3.file_name_biotransf_nap})."
     run_biotransformer3.markdown_link_biotransf_sirius = f"[View/Download the vm_NAP BioTransformer results for SIRIUS from {output_name}](./{run_biotransformer3.file_name_biotransf_sirius})."
