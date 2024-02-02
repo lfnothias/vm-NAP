452cba0d075a3577df09d35117f42714083c2508
Lucas Pradi <155846495+lucaspradi@users.noreply.github.com>
2024-02-02 14:19:17 +0100

    Corrected misspelling

--- src/vm_NAP_processing.py
+++ src/vm_NAP_processing.py
@@ -6,7 +6,7 @@ import os
 import sys
 from datetime import datetime
 import pandas as pd
-from IPhyton.display import display, Markdown
+from IPython.display import display, Markdown
 import re
 import subprocess
 
